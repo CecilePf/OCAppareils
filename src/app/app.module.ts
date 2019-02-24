f676848871e92a13869ad419f000d49baf14d240
c.pfister <c.pfister@neobridge.fr>
2019-02-24 20:06:55 +0100

    firt commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { PremierComponentComponent } from './premier-component/premier-component.component';
+import { AppareilComponent } from './appareil/appareil.component';
+import { AppareilService } from './services/appareil.service';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        PremierComponentComponent,
+        AppareilComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+    providers: [
+        AppareilService,
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/appareil/appareil.component.ts
@@ -0,0 +1,44 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { removeDebugNodeFromIndex } from '@angular/core/src/debug/debug_node';
+import { AppareilService } from '../services/appareil.service';
+
+
+@Component({
+    selector: 'app-appareil',
+    templateUrl: './appareil.component.html',
+    styleUrls: ['./appareil.component.scss']
+})
+export class AppareilComponent implements OnInit {
+  
+    @Input() appareilName: string;
+    @Input() appareilStatus: string;
+    @Input() index: number;
+
+    constructor(private appareilService: AppareilService) {
+
+    }
+
+    ngOnInit() {
+    }
+
+    getStatus() {
+        return this.appareilStatus;
+    }
+
+    getColor() {
+        if (this.appareilStatus === 'éteint') {
+        return 'red';
+        } else {
+        return 'green';
+        }
+    }
+
+    onSwitch() {
+        if(this.appareilStatus === 'allumé') {
+          this.appareilService.switchOffOne(this.index);
+        } else if(this.appareilStatus === 'éteint') {
+          this.appareilService.switchOnOne(this.index);
+        }
+    }
+
+}
