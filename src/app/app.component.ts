c108daec8e6408ae35be22d80e7fe4eb8e67e5eb
c.pfister <c.pfister@neobridge.fr>
2019-04-02 21:23:43 +0200

    add component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, interval, Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
 
+	secondes: number;
+	counterSubscription: Subscription;
     isAuth = false;
     
-    constructor() {
-    
-    } 
+    constructor() {} 
+
+    ngOnInit() {
+		const counter = interval(1000);
+
+		// suscribe() prend en arguments 3 fonctions anonymes
+		// 1 : se déclenche quand l'observable émet de nouvelles données, reçoit des données
+		// comme argument
+		// 2 : se déclenche si l'observale émet une erreur, reçoit une erreur comme
+		// argument
+		// 3 : se déclenche si l'observable s'achève, pas d'argument
+		this.counterSubscription = counter.subscribe(
+			(value) => {
+				this.secondes = value;
+			},
+			(error) => {
+				console.log('Uh-oh, an error occurred! : ' + error);
+			},
+			() => {
+				console.log('Observable complete!');
+			}
+		);
+	}
+	
+	ngOnDestroy() {
+		this.counterSubscription.unsubscribe();
+	}
 }
